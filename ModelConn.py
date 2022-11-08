17b8974bcfde7cb4ab685ed6d7492598facf240c
Alexandre F Silva <alexsilva.ts@gmail.com>
2022-11-07 21:31:00 -0300

    Update ModelConn.py

--- ModelConn.py
+++ ModelConn.py
@@ -9,7 +9,7 @@ class ClienteModel():
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'bancodeteste'
 
 mysql = MySQL(app)
 
